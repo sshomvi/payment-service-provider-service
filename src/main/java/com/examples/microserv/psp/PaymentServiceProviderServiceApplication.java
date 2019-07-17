13fd45a20beb6259b2b2d8765ccbb8428a91e1c8
sshomvi <sshomvi@gmail.com>
2019-07-17 17:49:03 +0530

    Initial commit

---
+++ src/main/java/com/examples/microserv/psp/PaymentServiceProviderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.examples.microserv.psp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceProviderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceProviderServiceApplication.class, args);
+	}
+
+}
